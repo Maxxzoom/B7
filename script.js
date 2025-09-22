5d722aea8c7025c3f6a434b05d0359bf0cb606e6
UmeshGardas <umeshgardas123@gmail.com>
2025-09-22 14:01:14 +0530

    html css done

---
+++ script.js
@@ -0,0 +1,84 @@
+// var a = 67;
+// let b = 45;
+// const c = true;
+// console.log(a);
+
+// function add() {
+//   console.log("addition");
+// }
+// add();
+
+// variables
+// var - global scope
+// we can reassign the value and redeclare the variable
+
+// var a = 34;
+// var a = 56;
+
+// function add() {
+//   var a = 67;
+//   console.log(a);
+// }
+
+// add();
+// console.log(a);
+// let
+// let is block scope
+// we cannot redeclare a variable but we can reassign the value
+
+// let b = 99;
+// b=57847;
+
+// function add2() {
+//   let b = 33;
+
+//   console.log(b);
+// }
+
+// add2();
+// console.log(b);
+
+// const
+// const is a block scope
+// we cannot reassign the value and we cannot redeclare the variable
+// const c = 88;
+// c=48598;
+// function add3() {
+//   const c = 11;
+//   console.log(c);
+// }
+// add3();
+// console.log(c);
+
+
+
+
+// Data Types
+// - Primitive Data Type
+// Number,String,null,undefined,BigInt,True/False
+
+
+// Number =378.58;
+// String = "umesh";
+// null =null;
+// undefined= "undefined";
+// BigInt=65789463567894504837;
+// Boolean=True/False
+
+
+// Non Primitive
+// -Object, Array [no limit]
+
+let student=[];
+console.log(typeof student);
+
+
+if(isNaN(34)){
+  console.log("Is Number");
+}
+else{
+  console.log("");  
+}
+ 
+// Functions
+
