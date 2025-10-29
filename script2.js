5a1d47f4b6ab2fbc022956c27c323842649e3b67
UmeshGardas <umeshgardas123@gmail.com>
2025-10-29 12:14:59 +0530

    revising the js

---
+++ script2.js
@@ -0,0 +1,88 @@
+// let a = 2,
+//   b = 3,
+//   c = 7;
+
+// console.log(a);
+var a = 20;
+// // Async
+// setTimeout(() => {
+//   console.log("setTimout");
+// }, 1000);
+// setTimeout(() => {
+//   console.log("setTimout2");
+// }, 1000);
+// // setInterval(() => {});
+
+// console.log(b);
+// console.log(c);
+
+// function add() {
+//   console.log(a);
+// }
+
+// var a = "yash";
+// // global scope
+
+// let b = null;
+// console.log(b);
+
+// const PI = 3.14;
+// block scope
+
+// if (data === undefined) {
+//   console.log("error in db");
+// }
+
+// Data types
+
+// Primite
+// String
+// Number
+// Boolean
+// BigInt
+// Symbol
+// undefined
+// null
+
+// Non primitive
+// Array, Objects
+
+let arr = [];
+let std1 = {};
+
+//normal func
+// function add(f, s) {
+//   return f + s;
+// }
+// // Arrow func
+// const add2 = (f, s) => f + s;
+
+// IIF
+// (function () {
+//   console.log("IIFE");
+// })();
+
+// // Callback
+// const add3 = () => {};
+
+// console.log(add(3, 5));
+// console.log(add2(6, 8));
+
+let arr2 = [3, "priyanka", "sudanshu", "umesh", true];
+
+// 5 < 5;
+// for (let i = 5; i < arr2.length; i++) {
+//   console.log(i);
+
+// }
+
+j = 0;
+while (j < arr2.length) {
+  console.log(arr2[j]);
+  j++;
+}
+
+// Array
+for (let a of arr2) {
+  console.log(a);
+}
